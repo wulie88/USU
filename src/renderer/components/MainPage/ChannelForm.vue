959b25795f4162c29d3051f44d32b3d7d07deea6
Leo <wulie88@gmail.com>
2017-09-26 09:01:42 +0800

    加入无功能通道

--- src/renderer/components/MainPage/ChannelForm.vue
+++ src/renderer/components/MainPage/ChannelForm.vue
@@ -1,16 +1,17 @@
 <template>
   <form>
   	<label>配置: {{name}}</label>
-	<div class="form-group">
-		<label>通道功能</label>
-		<select v-model="type" class="form-control">
-		  <option value="1">开关</option>
-		  <option value="2">开关/调光</option>
-		  <option value="3">窗帘</option>
-		</select>
-	</div>
-	<!-- 开关 -->
-	<template v-if="type == '1'">
+  	<div class="form-group">
+  		<label>通道功能</label>
+  		<select v-model="type" class="form-control">
+        <option value="0">无</option>
+  		  <option value="1">开关</option>
+  		  <option value="2">开关/调光</option>
+  		  <option value="3">窗帘</option>
+  		</select>
+  	</div>
+	  <!-- 开关 -->
+	  <template v-if="type == '1'">
 	    <div class="form-group">
 	      <label>长短按键区分</label>
 	      <select v-model="button.distinction" class="form-control">
@@ -20,68 +21,68 @@
 	    </div>
 	    <!-- 不区分 -->
 	    <template v-if="button.distinction == '0'">
-	      	<div class="form-group">
-	      		<label>触点闭合动作（上升沿）</label>
-	            <select v-model="button.withoutDistinction.r1" class="form-control">
-	              <option value="0">开启</option>
-			      <option value="1">关闭</option>
-			      <option value="2">反转</option>
-			      <option value="3">无动作</option>
-			    </select>
-	      	</div>
-	      	<div class="form-group">
-	      		<label>触点打开动作（下降沿）</label>
-	            <select v-model="button.withoutDistinction.r2" class="form-control">
-	              <option value="0">开启</option>
-			      <option value="1">关闭</option>
-			      <option value="2">反转</option>
-			      <option value="3">无动作</option>
-			    </select>
-	      	</div>
+      	<div class="form-group">
+      		<label>触点闭合动作（上升沿）</label>
+            <select v-model="button.withoutDistinction.r1" class="form-control">
+              <option value="0">开启</option>
+		      <option value="1">关闭</option>
+		      <option value="2">反转</option>
+		      <option value="3">无动作</option>
+		    </select>
+      	</div>
+      	<div class="form-group">
+      		<label>触点打开动作（下降沿）</label>
+            <select v-model="button.withoutDistinction.r2" class="form-control">
+              <option value="0">开启</option>
+		      <option value="1">关闭</option>
+		      <option value="2">反转</option>
+		      <option value="3">无动作</option>
+		    </select>
+      	</div>
 	    </template>
 	    <!-- 区分 -->
 	    <template v-else>
-	      	<div class="form-group">
-	      		<label>节点类型</label>
-	            <select v-model="button.withDistinction.r0" class="form-control">
-	              <option value="0">常闭</option>
-			      <option value="1">常开</option>
-			    </select>
-	      	</div>
-	      	<div class="form-group">
-	      		<label>短按反应</label>
-	            <select v-model="button.withDistinction.r1" class="form-control">
-	              <option value="0">开启</option>
-			      <option value="1">关闭</option>
-			      <option value="2">反转</option>
-			      <option value="3">无动作</option>
-			    </select>
-	      	</div>
-	      	<div class="form-group">
-	      		<label>长按反应</label>
-	            <select v-model="button.withDistinction.r2" class="form-control">
-	              <option value="0">开启</option>
-			      <option value="1">关闭</option>
-			      <option value="2">反转</option>
-			      <option value="3">无动作</option>
-			    </select>
-	      	</div>
-			<div class="form-group">
-				<label>长按反应时间2-255(单位100ms)</label>
-				<input  v-model="button.withDistinction.r4" type="text" class="form-control">
-			</div>
-	      	<div class="form-group">
-	      		<label>长短按键操作对象</label>
-	            <select v-model="button.withDistinction.r5" class="form-control">
-	              <option value="0">1个</option>
-			      <option value="1">2个</option>
-			    </select>
-	      	</div>
+      	<div class="form-group">
+      		<label>节点类型</label>
+            <select v-model="button.withDistinction.r0" class="form-control">
+              <option value="0">常闭</option>
+		      <option value="1">常开</option>
+		    </select>
+      	</div>
+      	<div class="form-group">
+      		<label>短按反应</label>
+            <select v-model="button.withDistinction.r1" class="form-control">
+              <option value="0">开启</option>
+		      <option value="1">关闭</option>
+		      <option value="2">反转</option>
+		      <option value="3">无动作</option>
+		    </select>
+      	</div>
+      	<div class="form-group">
+      		<label>长按反应</label>
+            <select v-model="button.withDistinction.r2" class="form-control">
+              <option value="0">开启</option>
+		      <option value="1">关闭</option>
+		      <option value="2">反转</option>
+		      <option value="3">无动作</option>
+		    </select>
+      	</div>
+  			<div class="form-group">
+  				<label>长按反应时间2-255(单位100ms)</label>
+  				<input  v-model="button.withDistinction.r4" type="text" class="form-control">
+  			</div>
+      	<div class="form-group">
+      		<label>长短按键操作对象</label>
+            <select v-model="button.withDistinction.r5" class="form-control">
+              <option value="0">1个</option>
+		      <option value="1">2个</option>
+		    </select>
+      	</div>
 	    </template>
-	</template>
+	  </template>
 
-	<!-- 开关/调光 -->
-	<template v-else-if="type == '2'">
+	  <!-- 开关/调光 -->
+	  <template v-else-if="type == '2'">
 	    <div class="form-group">
 	      <label>节点类型</label>
 	      <select v-model="dimmer.r0" class="form-control">
@@ -96,31 +97,31 @@
 	        <option value="1">只调光</option>
 	      </select>
 	    </div>
-      	<div class="form-group">
-      		<label>短按反应</label>
-            <select v-model="dimmer.r2" class="form-control">
-              <option value="0">开启</option>
-		      <option value="1">关闭</option>
-		      <option value="2">反转</option>
-		      <option value="3">无动作</option>
-		    </select>
-      	</div>
-      	<div class="form-group">
-      		<label>长按反应</label>
-            <select v-model="dimmer.r3" class="form-control">
-              <option value="0">亮度增加</option>
-		      <option value="1">亮度减小</option>
-		      <option value="2">亮度循环</option>
-		    </select>
-      	</div>
-		<div class="form-group">
-			<label>长按反应时间3-255(单位100ms)</label>
-			<input  v-model="dimmer.r5" type="text" class="form-control">
-		</div>
-	</template>
+    	<div class="form-group">
+    		<label>短按反应</label>
+          <select v-model="dimmer.r2" class="form-control">
+            <option value="0">开启</option>
+	      <option value="1">关闭</option>
+	      <option value="2">反转</option>
+	      <option value="3">无动作</option>
+	    </select>
+    	</div>
+    	<div class="form-group">
+    		<label>长按反应</label>
+          <select v-model="dimmer.r3" class="form-control">
+            <option value="0">亮度增加</option>
+	      <option value="1">亮度减小</option>
+	      <option value="2">亮度循环</option>
+	    </select>
+    	</div>
+  		<div class="form-group">
+  			<label>长按反应时间3-255(单位100ms)</label>
+  			<input  v-model="dimmer.r5" type="text" class="form-control">
+  		</div>
+  	</template>
 
-	<!-- 窗帘 -->
-	<template v-else-if="type == '3'">
+	  <!-- 窗帘 -->
+	  <template v-else-if="type == '3'">
 	    <div class="form-group">
 	      <label>节点类型</label>
 	      <select v-model="curtain.r1" class="form-control">
@@ -128,19 +129,19 @@
 	        <option value="1">常闭</option>
 	      </select>
 	    </div>
-      	<div class="form-group">
-      		<label>长按反应</label>
-            <select v-model="curtain.r3" class="form-control">
-              <option value="0">上升</option>
-		      <option value="1">下降</option>
-		      <option value="2">反转</option>
-		    </select>
-      	</div>
-		<div class="form-group">
-			<label>长按反应时间3-255(单位100ms)</label>
-			<input  v-model="curtain.r4" type="text" class="form-control">
-		</div>
-	</template>
+    	<div class="form-group">
+    		<label>长按反应</label>
+          <select v-model="curtain.r3" class="form-control">
+            <option value="0">上升</option>
+	      <option value="1">下降</option>
+	      <option value="2">反转</option>
+	    </select>
+    	</div>
+  		<div class="form-group">
+  			<label>长按反应时间3-255(单位100ms)</label>
+  			<input  v-model="curtain.r4" type="text" class="form-control">
+  		</div>
+  	</template>
 
     <footer class="form-footer">
       <label>修改后请点击保存</label>
@@ -211,7 +212,9 @@
          */
         let ds = []
         let os = []
-        if (this.type === '1') {
+        if (this.type === '0') {
+          ds = [0, 0, 0, 0]
+        } else if (this.type === '1') {
           if (this.button.distinction === '0') {
             // 开关--不区分
             ds = [0].concat(Device.merge(this.button.withoutDistinction, 7))
